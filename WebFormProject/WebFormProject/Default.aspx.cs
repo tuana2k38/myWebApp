0b3d59959887ecc01a48130837e46f868824207d
HuyTuan <tuannguyenhuy@vccorp.vn>
2021-07-09 10:22:17 +0700

    UserControl

--- WebFormProject/WebFormProject/Default.aspx.cs
+++ WebFormProject/WebFormProject/Default.aspx.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using WebFormProject.UserControl;
 namespace WebFormProject
 {
     public partial class _Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            UserInfoBoxControl userInfoBoxControl = (UserInfoBoxControl)LoadControl("~/UserControl/UserInfoBoxControl.ascx");
+            userInfoBoxControl.UserName = "John Doe";
+            userInfoBoxControl.UserAge = 78;
+            userInfoBoxControl.UserCountry = "Spain";
+            phUserInfoBox.Controls.Add(userInfoBoxControl);
+
 
         }
     }
